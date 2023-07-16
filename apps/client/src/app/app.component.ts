314765ba161571bbf7bf09440aac827006104e44
Adrian Zinko <zinko.adrian00@gmail.com>
2023-07-16 08:10:47 +0200

    chore: add styles config, reset default agent styling

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sdeck-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `<router-outlet />`,
 })
-export class AppComponent {
-  title = 'client';
-}
+export class AppComponent {}
